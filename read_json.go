0a4e9fc27ec9dc79d98b9c8e0017dd4813a36163
Mike Rybka <merybka@gmail.com>
2024-01-29 12:11:58 -0500

    Handle 404

--- read_json.go
+++ read_json.go
@@ -3,17 +3,21 @@ package files
 import (
 	"bytes"
 	"encoding/json"
+	"strings"
 
 	"github.com/brass-software/api"
 )
 
 func ReadJSON[T any](path string) (*T, error) {
+	var v T
 	buf := bytes.NewBuffer(nil)
 	err := api.GET(buf, path)
 	if err != nil {
+		if strings.HasPrefix(err.Error(), "404") {
+			return nil, nil
+		}
 		return nil, err
 	}
-	var v T
 	err = json.NewDecoder(buf).Decode(&v)
 	if err != nil {
 		return nil, err
